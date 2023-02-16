9cd03e87c5398b23262e61ec04b44cbe83645569
RenatoConceicao27 <contreirasconceicao@gmail.com>
2023-02-16 01:51:02 +0000

    portfolio smooth scroll fixe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
                 </p>
 
                 <div>
-                    <Link to="portfolio" 
+                    <Link to="Portfolio" 
                     smooth 
                     duration className= "group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
                         Portfolio
